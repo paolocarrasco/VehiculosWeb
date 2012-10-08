50141706ac83eaff41a665fdd36324234f8fffdd
Paolo Carrasco <paolo.carrasco@gmail.com>
2012-10-07 23:23:36 -0500

    se corrige la declaracion del   vehiculodaoimpl se pone como atributo de objeto y se inicializa en su declaracion

--- Codigo/VehiculosWeb/src/java/pe/com/abaris/vehiculos/gestores/GestorVehiculoImpl.java
+++ Codigo/VehiculosWeb/src/java/pe/com/abaris/vehiculos/gestores/GestorVehiculoImpl.java
@@ -8,8 +8,10 @@ import pe.com.abaris.vehiculos.modelo.Vehiculo;
  * Contiene la logica relacionada a los vehiculos
  */
 public class GestorVehiculoImpl {
+    
+    private VehiculoDaoImpl vehiculoDao = new VehiculoDaoImpl();
 
     public List<Vehiculo> listar() {
-        return new VehiculoDaoImpl().buscar();
+        return vehiculoDao.buscar();
     }
 }
